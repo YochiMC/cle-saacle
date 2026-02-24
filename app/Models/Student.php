9893c827c98bee61b4a57afaa3a68d6d458d1839
adonai76 <21240785@leon.tecnm.mx>
2026-02-24 09:36:07 -0600

    hasFactory Students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
-
+    use HasFactory;
     protected $fillable = [
         'firstName',
         'lastName',
