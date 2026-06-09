94e557db0a678241914423002f3be96e8740fe5c
Victor <victaz20136@gmail.com>
2026-06-09 10:27:18 -0600

    Logos

--- resources/views/certificates/otra-institucion.blade.php
+++ resources/views/certificates/otra-institucion.blade.php
@@ -10,7 +10,7 @@ Oficio No. CLE-CNV-<span class="highlight">{{ $no_oficio }}</span>/{{ date('Y') 
     <b>{{ $estatus }} {{ $nombre }}</b> con Número de Control <b>{{ $numero_control }}</b>,
     de la carrera de <b>{{ $carrera }}</b> con clave del Plan de Estudio <b>{{ $plan_estudios }}</b>,
     presentó la documentación de acreditación emitida por otra institución, acreditando el equivalente
-    al nivel B1 (<b>{{ $nivel }}</b>) con base al Marco Común Europeo de Referencia (MCER),
+    al nivel (<b>{{ $nivel }}</b>) con base al Marco Común Europeo de Referencia (MCER),
     acorde a la documentación existente en los archivos de este departamento.
 </div>
 @endsection
